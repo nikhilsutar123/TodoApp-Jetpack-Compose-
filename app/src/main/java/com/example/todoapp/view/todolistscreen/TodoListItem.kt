ed4d1ffe856d0452df5ab18d0bcaf685814c86df
nikhil sutar <nikhil.sutar@viraltech.in>
2024-10-30 18:19:45 +0530

    rename class

---
+++ app/src/main/java/com/example/todoapp/view/todolistscreen/TodoListItem.kt
@@ -0,0 +1,71 @@
+package com.example.todoapp.view.todolistscreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
+import com.example.todoapp.data.Todo
+
+@Composable
+fun TodoListScreen(
+    todo: Todo,
+    onEvent: (TodoListEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+    ) {
+        Column(modifier = Modifier.weight(1f), verticalArrangement = Arrangement.SpaceEvenly) {
+            Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = todo.title,
+                    fontSize = TextUnit(value = 20f, type = TextUnitType.Sp)
+                )
+                Checkbox(
+                    checked = todo.isDone,
+                    onCheckedChange = { isChecked ->
+                        onEvent(
+                            TodoListEvent.OnDoneChange(
+                                todo,
+                                isChecked
+                            )
+                        )
+                    })
+            }
+            todo.desc?.let {
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = it,
+                    fontSize = TextUnit(value = 15f, type = TextUnitType.Sp)
+                )
+            }
+            HorizontalDivider(
+                thickness = 10.dp,
+                color = Color.DarkGray,
+                modifier = Modifier.padding(8.dp)
+            )
+            TextButton(onClick = {
+                onEvent(TodoListEvent.OnDeleteTodoClick(todo))
+            }) {
+                Text(text = "Delete")
+            }
+        }
+    }
+}
